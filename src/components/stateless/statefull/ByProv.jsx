4912d2c9e28de4ae5a19ec813d1d089e0b59ffab
Franz Aditya Natanael Sinaga <sinagafranz12@gmail.com>
2022-04-11 21:48:21 +0700

    change api and more enhancement in ui, and add formatter to number

--- src/components/stateless/statefull/ByProv.jsx
+++ src/components/stateless/statefull/ByProv.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React, { Component } from 'react';
 import API from '../../../services/api';
 
-class ByProv extends Component{
+import Formatter from '../../../utils/Formatter';
+class ByProv extends Component {
     state = {
         dataByProv: [],
         filterProv: [],
@@ -11,29 +13,28 @@ class ByProv extends Component{
     getByProv = () => {
         API.getCasesByProv().then((res) => {
             this.setState({
-                dataByProv: res.data,
-                filterProv: res.data
+                dataByProv: res,
+                filterProv: res
             })
-            // console.log(this.state.dataByProv);
         })
     }
 
-    resetText(){
+    resetText() {
         document.getElementById('provinsi-input').value = "";
         this.setState({
             dataByProv: this.state.filterProv
         })
     }
-    
-    filterByProv(provinsi){
-        if (provinsi.target.value == "") {
+
+    filterByProv(provinsi) {
+        if (provinsi.target.value === "") {
             this.setState({
                 dataByProv: this.state.filterProv
             })
         } else {
             let dataByProv = [...this.state.filterProv]
             dataByProv = []
-            this.state.dataByProv.filter(cond => cond.provinsi.includes(provinsi.target.value)).map((data) => {
+            this.state.dataByProv.filter(cond => cond.provinsi.includes(provinsi.target.value.toUpperCase())).map((data) => {
                 dataByProv.push(data)
                 this.setState({
                     dataByProv,
@@ -42,26 +43,26 @@ class ByProv extends Component{
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getByProv()
     }
 
-    render(){
-        return(
-            <div style={{marginTop: "30px"}}>
+    render() {
+        return (
+            <div style={{ marginTop: "30px" }}>
                 <h3>Kasus Berdasarkan Provinsi</h3>
-                <div className="col-md-4" style={{paddingLeft: "0px"}}>
+                <div className="col-md-4" style={{ paddingLeft: "0px" }}>
                     <div className="input-group mb-3">
-                        <input type="text" id="provinsi-input" className="form-control" onChange={(text) => this.filterByProv(text)} placeholder="Provinsi"/>
+                        <input type="text" id="provinsi-input" className="form-control" onChange={(text) => this.filterByProv(text)} placeholder="Provinsi" />
                         <div className="input-group-append">
                             <button className="btn btn-warning" type="button" onClick={() => this.resetText()}>❌</button>
                         </div>
                     </div>
                 </div>
-                
+
                 <div className="table-wrapper-scroll-y my-custom-scrollbar table-responsive">
-                    <table className="table table-striped table-dark table-borderless">
-                        <thead>
+                    <table className="table table-striped table-dark table-brderless">
+                        <thead style={{ width: "100%" }}>
                             <tr>
                                 <th>Provinsi</th>
                                 <th>Positif</th>
@@ -70,24 +71,25 @@ class ByProv extends Component{
                             </tr>
                         </thead>
                         <tbody>
-                        {
-                            this.state.dataByProv.map(data => {
-                                return (
-                                    <tr key={data.fid}>
-                                        <td>{data.provinsi}</td>
-                                        <td><span className="badge badge-primary">{data.kasusPosi}</span></td>
-                                        <td><span className="badge badge-danger">{data.kasusMeni}</span></td>                                            <td><span className="badge badge-success">{data.kasusSemb}</span></td>
-                                    </tr>
-                                )
-                            })
-                        }
+                            {
+                                this.state.dataByProv.map(data => {
+                                    return (
+                                        <tr key={data.provinsi}>
+                                            <td>{data.provinsi}</td>
+                                            <td><span className="badge badge-primary">{Formatter.numberWithCommas(data.dirawat)}</span></td>
+                                            <td><span className="badge badge-danger">{Formatter.numberWithCommas(data.meninggal)}</span></td>
+                                            <td><span className="badge badge-success">{Formatter.numberWithCommas(data.sembuh)}</span></td>
+                                        </tr>
+                                    )
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
-                <br/>
-                <br/>
-            </div>
-        ) 
+                <br />
+                <br />
+            </div >
+        )
     }
 }
 
--- src/services/config.js
+++ src/services/config.js
@@ -1,1 +1,2 @@
-export const rootPath = "https://indonesia-covid-19.mathdro.id/api"
+// export const rootPath = "https://indonesia-covid-19.mathdro.id/api"
+export const rootPath = "https://apicovid19indonesia-v2.vercel.app/api";
