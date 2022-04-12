76fe8c6c2c52805a4b8762246e59b2837e49204c
Franz Aditya Natanael Sinaga <sinagafranz12@gmail.com>
2022-04-12 20:08:59 +0700

    Updates

---
+++ precache-manifest.05af93defe77f41b5decd8aa91554353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3466bfcebe75202286b8151ab8a230b6",
+    "url": "/SimpleIndonesiaCoronavirus/index.html"
+  },
+  {
+    "revision": "63617b3e6ca60588aba7",
+    "url": "/SimpleIndonesiaCoronavirus/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3dffd73fd24d10f5879d",
+    "url": "/SimpleIndonesiaCoronavirus/static/css/main.f97a021f.chunk.css"
+  },
+  {
+    "revision": "63617b3e6ca60588aba7",
+    "url": "/SimpleIndonesiaCoronavirus/static/js/2.d47ce072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SimpleIndonesiaCoronavirus/static/js/2.d47ce072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dffd73fd24d10f5879d",
+    "url": "/SimpleIndonesiaCoronavirus/static/js/main.86fd7d58.chunk.js"
+  },
+  {
+    "revision": "f5c4272febb456b4e621",
+    "url": "/SimpleIndonesiaCoronavirus/static/js/runtime-main.b83101cf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00fcc758019816bcc18c594d8a385092.js"
+  "/SimpleIndonesiaCoronavirus/precache-manifest.05af93defe77f41b5decd8aa91554353.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SimpleIndonesiaCoronavirus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
